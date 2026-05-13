d66ef57adafea360cf749b86baed20b5cbeae1db
adding files <abhishek.20ec106@jssaten.ac.in>
2026-05-13 10:39:58 +0530

    adding dto pattern

--- src/main/java/com/app/ecom/controller/UserController.java
+++ src/main/java/com/app/ecom/controller/UserController.java
@@ -1,5 +1,8 @@
-package com.app.ecom;
+package com.app.ecom.controller;
 
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
+import com.app.ecom.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +19,26 @@ public class UserController {
 
     @GetMapping
 //    @RequestMapping(value = "api/users", method = RequestMethod.GET)
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
         return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
         return userService.fetchUsers(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        userService.addUser(user);
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
+        userService.addUser(userRequest);
         return ResponseEntity.ok("User added successfully");
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable Long id,@RequestBody User updatedUser) {
-        boolean updated = userService.updateUser(id, updatedUser);
+    public ResponseEntity<String> updateUser(@PathVariable Long id,@RequestBody UserRequest updatedUserRequest) {
+        boolean updated = userService.updateUser(id, updatedUserRequest);
         if (updated) {
         return ResponseEntity.ok("User updated successfully");
         }
--- src/main/java/com/app/ecom/model/User.java
+++ src/main/java/com/app/ecom/model/User.java
@@ -1,9 +1,13 @@
-package com.app.ecom;
+package com.app.ecom.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity(name = "user_table")
@@ -18,7 +22,15 @@ public class User {
     private String lastName;
     private String email;
     private String phone;
-
-//    @Enumerated(EnumType.STRING)
     private UserRole role = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
